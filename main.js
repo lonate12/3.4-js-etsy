cdac9a03bdb5c49177be634b0f28b1f5756e11ed
Luis Rene Onate <lonate12@gmail.com>
2016-09-29 13:52:15 -0400

    2 down.

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-var furniture = ['chair', 'desk', 'lamp'];
+(function(){
+ 'use strict';
+  var sum = items.reduce(function(total, current){
+    total += current.price;
+    return total;
+  }, 0);
 
-function forEach(array, callback){
-  for(var i=0; i< array.length; i++){
-    callback(array[i], i, array);
-  }
-}
+  var averagePrice = (sum/items.length).toFixed(2);
+  console.log('The average price is: $' + averagePrice);
+
+  var filteredItems = items.filter(function(thing){
+    var price = thing.price;
+    return (price < 18.00) && (price > 14.00);
+  });
+
+  // filteredItems.forEach(function(object){
+  //   console.log(object.title);
+  // });
+
+  console.log("Items that cost between $14.00 USD and $18.00 USD: " +
+    filteredItems[0].title + ' ,' + filteredItems[1].title + ' , and ' +
+    filteredItems[2].title);
+
+    
+}());
